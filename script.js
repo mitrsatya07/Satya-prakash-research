2fc14fc141c8e71ded8e45a2cc5f954effab5f72
mitrsatya07 <satyaprakashkumawat07@gmail.com>
2025-07-13 21:22:05 +0100

    initial upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.addEventListener('contextmenu', e => e.preventDefault());
+document.addEventListener('copy', e => e.preventDefault());
+document.addEventListener('cut', e => e.preventDefault());
